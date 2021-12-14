bc2e4758ef4492fdc42ab817c9f6e81a9521450b
Deun Lee <54697735+deunlee@users.noreply.github.com>
2021-12-14 13:33:58 +0900

    Update User.java

--- src/main/tetris/entity/user/User.java
+++ src/main/tetris/entity/user/User.java
@@ -1,27 +1,26 @@
 package main.tetris.entity.user;
 
-public class User {
-    private String id;
-    private String password;
-    private String name;
-    private int    bestScore;
+import java.io.Serializable;
 
-    public User(String id, String password, String name, int bestScore) {
-        this.id        = id;
-        this.password  = password;
+public class User implements Serializable {
+    private final String name;
+    private final String password;
+    private int          bestScore;
+
+    public User(final String name, final String password, int bestScore) {
         this.name      = name;
+        this.password  = password;
         this.bestScore = bestScore;
     }
 
-    public String getId()     { return id;        }
-    public String getName()   { return name;      }
-    public int getBestScore() { return bestScore; }
+    public String getName()      { return name;      }
+    public int    getBestScore() { return bestScore; }
 
     public void setBestScore(int score) {
         bestScore = score;
     }
 
-    public boolean checkPassword(String password) {
+    public boolean checkPassword(final String password) {
         return this.password.equals(password);
     }
 }
